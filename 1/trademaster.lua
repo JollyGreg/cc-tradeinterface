001c06abc9969016994dae9897eaee19cf85efa2
JollyGreg <139110078+JollyGreg@users.noreply.github.com>
2025-02-22 15:53:10 +0800

    Update trademaster.lua

--- 1/trademaster.lua
+++ 1/trademaster.lua
@@ -2,14 +2,26 @@
 local function main()
     local trades = {}
     local peripherals = peripheral.getNames()
+
+    local tradeinterfaces = {}
+    local storage = ""
+
     for _, peripheral_ in pairs(peripherals) do
         if peripheral.hasType(peripheral_, "trading_interface") then
             print(peripheral_)
-            
-            local id = multishell.launch({}, "/tradeinterface.lua", peripheral_)
-            multishell.setTitle(id, peripheral_)
+            table.insert(tradeinterfaces, peripheral_)
+
+        elseif peripheral.hasType(peripheral_, "minecraft:chest") then
+            print(peripheral_)
+            storage = peripheral_
         end
     end
+
+    for _, interface in pairs(tradeinterfaces) do
+        local id = multishell.launch({}, "/tradeinterface.lua", {interface = interface, storage = storage})
+        multishell.setTitle(id, interface)
+    end
+
 end
 main()
 
